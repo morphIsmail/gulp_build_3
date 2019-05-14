11535ba441b8fe07949247feb64163cc8718121a
morphIsmail <ismail_2016@mail.ru>
2019-05-14 12:29:47 +0300

    Сборка проекта на Gulp 4 Версия 3, Препроцессоры и Изображения

---
+++ README.md
@@ -0,0 +1,37 @@
+# Сборка проекта на Gulp 3.0 
+
+## Описание проекта: 
+С помощью этих файлов вы сможете быстро настроить сборку вашего проекта на Gulp. 
+
+## Структура файлов и папок:  
+>./src  
+>>	/css  
+>>>		/*.scss *.sass *.less *.styl *.css 
+>>>		/*.scss *.sass *.less *.styl *.css 
+>> /img  
+>>	/js  
+>>>		/lib.js  
+>>>		/main.js  
+>./gulpfile.js  
+>./package.json  
+>./index.html  
+
+## Инструкция:  
+1. Скачать все файлы в любую директорию   
+2. Ввести в терминале/командной строке команду: npm i (должен быть установлен node.js) 
+3. Выполнить команду: gulp (запуск таска default, который очистит каталог build и запустит таск scripts, styles и img-compress, а так же watch - отслеживает изменения в файлах html, css, sass, scss, less, styl, js и в каталоге img) 
+4. Писать свой код и наслаждаться автоматической сборкой проекта. 
+
+## Ссылки:
+* __YouTube канал ITDoctor:__ https://www.youtube.com/c/ITDoctor
+* __Репозиторий GitHub:__ https://github.com/morphIsmail/gulp_build_2
+* __Сайт ITDoctor:__ http://itdoctor.info/
+* __Группа в ВК:__ https://vk.com/itdoctorstudio
+* __Мой Twitter:__ https://twitter.com/ITDoctor_morph
+
+## Для поддержки развития проекта:
+__Сбербанк VISA:__ 4276 5200 1409 4318  
+__Yandex:__ 410011260821995 - https://yasobe.ru/na/itdoctor  
+__QIWI:__ 4890 4943 0383 5581  
+__WMR:__ R444308690108  
+__WMZ:__ Z608507028676  
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+//Подключаем галп
+const gulp = require('gulp');
+//Объединение файлов
+const concat = require('gulp-concat');
+//Добапвление префиксов
+const autoprefixer = require('gulp-autoprefixer');
+//Оптисизация стилей
+const cleanCSS = require('gulp-clean-css');
+//Оптимизация скриптов
+const uglify = require('gulp-uglify');
+//Удаление файлов
+const del = require('del');
+//Синхронизация с браузером
+const browserSync = require('browser-sync').create();
+//Для препроцессоров стилей
+const sourcemaps = require('gulp-sourcemaps');
+//Sass препроцессор
+const sass = require('gulp-sass');
+//Less препроцессор
+const less = require('gulp-less');
+//Stylus препроцессор
+const stylus = require('gulp-stylus');
+//Модуль для сжатия изображений
+const imagemin = require('gulp-imagemin');
+
+//Порядок подключения файлов со стилями
+const styleFiles = [
+   './src/css/color.scss',
+   './src/css/main.scss'
+]
+//Порядок подключения js файлов
+const scriptFiles = [
+   './src/js/lib.js',
+   './src/js/main.js'
+]
+
+//Таск для обработки стилей
+gulp.task('styles', () => {
+   //Шаблон для поиска файлов CSS
+   //Всей файлы по шаблону './src/css/**/*.css'
+   return gulp.src(styleFiles)
+      .pipe(sourcemaps.init())
+      //Указать stylus() , sass() или less()
+      .pipe(stylus())
+      //Объединение файлов в один
+      .pipe(concat('style.css'))
+      //Добавить префиксы
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions'],
+         cascade: false
+      }))
+      //Минификация CSS
+      .pipe(cleanCSS({
+         level: 2
+      }))
+      .pipe(sourcemaps.write('./'))
+      //Выходная папка для стилей
+      .pipe(gulp.dest('./build/css'))
+      .pipe(browserSync.stream());
+});
+
+//Таск для обработки скриптов
+gulp.task('scripts', () => {
+   //Шаблон для поиска файлов JS
+   //Всей файлы по шаблону './src/js/**/*.js'
+   return gulp.src(scriptFiles)
+      //Объединение файлов в один
+      .pipe(concat('script.js'))
+      //Минификация JS
+      .pipe(uglify({
+         toplevel: true
+      }))
+      //Выходная папка для скриптов
+      .pipe(gulp.dest('./build/js'))
+      .pipe(browserSync.stream());
+});
+
+//Таск для очистки папки build
+gulp.task('del', () => {
+   return del(['build/*'])
+});
+
+//Таск для сжатия изображений
+gulp.task('img-compress', ()=> {
+   return gulp.src('./src/img/**')
+   .pipe(imagemin({
+      progressive: true
+   }))
+   .pipe(gulp.dest('./build/img/'))
+});
+
+//Таск для отслеживания изменений в файлах
+gulp.task('watch', () => {
+   browserSync.init({
+      server: {
+         baseDir: "./"
+      }
+   });
+   //Следить за добавлением новых изображений
+   gulp.watch('./src/img/**', gulp.series('img-compress'))
+   //Следить за файлами со стилями с нужным расширением
+   gulp.watch('./src/css/**/*.scss', gulp.series('styles'))
+   //Следить за JS файлами
+   gulp.watch('./src/js/**/*.js', gulp.series('scripts'))
+   //При изменении HTML запустить синхронизацию
+   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+//Таск по умолчанию, Запускает del, styles, scripts, img-compress и watch
+gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts', 'img-compress'), 'watch'));
